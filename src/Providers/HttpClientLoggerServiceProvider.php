4e6985259605b2334ef79089b1ade7fed14c8c40
aniket-magadum-fyntune <aniketmagadum77@gmail.com>
2023-02-06 16:53:50 +0530

    Update HttpClientLoggerServiceProvider.php

--- src/Providers/HttpClientLoggerServiceProvider.php
+++ src/Providers/HttpClientLoggerServiceProvider.php
@@ -6,6 +6,14 @@ use Illuminate\Support\ServiceProvider;
 
 class HttpClientLoggerServiceProvider extends ServiceProvider
 {
+    protected $listen = [
+        'Illuminate\Http\Client\Events\ResponseReceived' => [
+            'AniketMagadum\HttpClientLogger\Listeners\LogResponseReceived',
+        ],
+        'Illuminate\Http\Client\Events\ConnectionFailed' => [
+            'AniketMagadum\HttpClientLogger\Listeners\LogConnectionFailed',
+        ],
+    ];
     /**
      * Bootstrap services.
      *
