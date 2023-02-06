954dd246e76590c394e65eba2ba8e492bd20b176
aniket-magadum-fyntune <aniketmagadum77@gmail.com>
2023-02-06 16:22:10 +0530

    Create HttpClientLoggerServiceProvider.php

---
+++ src/Providers/HttpClientLoggerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AniketMagadum\HttpClientLogger\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HttpClientLoggerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+}
